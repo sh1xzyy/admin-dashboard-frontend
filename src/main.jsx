980aa0f9768c2099592d112bc29a13862ef0b236
sh1xzyy <alekslungol@gmail.com>
2025-11-01 19:19:56 +0300

    base settings update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./app/App";
 import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
