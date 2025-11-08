731da6b83ae2f92fe9d249238630355f18e9ce33
sh1xzyy <alekslungol@gmail.com>
2025-11-08 14:52:08 +0300

    fix import

--- src/modules/products/AddProductButton/AddProductButton.jsx
+++ src/modules/products/AddProductButton/AddProductButton.jsx
@@ -2,7 +2,7 @@ import css from "./AddProductButton.module.css";
 import AddIcon from "../assets/add.svg?react";
 import { Button } from "@blueprintjs/core";
 
-const AddProductButton = () => {
+export const AddProductButton = () => {
   return (
     <div className={css.wrapper}>
       <Button className={css.addProductButton}>
@@ -12,5 +12,3 @@ const AddProductButton = () => {
     </div>
   );
 };
-
-export default AddProductButton;
